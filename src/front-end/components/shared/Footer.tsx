3491de5d1b972609f12bbc3453e17bf8c0ed4ebb
JesusLugo2002 <jesusdlugo02@gmail.com>
2026-02-10 12:05:15 +0000

    chore: changes on navbar and footer

--- src/front-end/components/shared/Footer.tsx
+++ src/front-end/components/shared/Footer.tsx
@@ -1,9 +1,28 @@
-import { Text, View } from "react-native";
+import { usePathname } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Footer() {
-  return (
-    <View>
-      <Text>ArepaConGofio © 2026</Text>
-    </View>
-  );
+  const pathname = usePathname();
+
+  if (pathname.includes("login") || pathname.includes("register")) {
+    return (
+      <View style={styles.footer}>
+        <Text style={styles.copyrightLabel}>ArepaConGofio © 2026</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    position: "fixed",
+    bottom: 0,
+    flex: 1,
+    width: "100%",
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  copyrightLabel: {
+    letterSpacing: 1,
+  },
+});
--- src/front-end/components/shared/Navbar.tsx
+++ src/front-end/components/shared/Navbar.tsx
@@ -1,27 +1,26 @@
-import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Navbar() {
-  const router = useRouter();
-
   return (
     <View style={styles.navbar}>
-      <Text style={styles.navbarTitle} onPress={() => router.navigate("/")}>
-        Steamgram
-      </Text>
+      <Link href="/">
+        <Pressable>
+          <Text style={styles.navbarTitle}>Steamgram</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   navbar: {
-    marginHorizontal: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    paddingVertical: 10,
     alignItems: "center",
   },
   navbarTitle: {
     fontSize: 20,
+    letterSpacing: 0.5,
     fontWeight: "bold",
   },
 });
