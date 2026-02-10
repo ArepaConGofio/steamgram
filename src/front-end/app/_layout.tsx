9053ac7d3f40d2d62e5a8bed41639d5f32e8dffc
JesusLugo2002 <jesusdlugo02@gmail.com>
2026-02-10 11:22:59 +0000

    feat: add ThemeProvider

--- src/front-end/app/_layout.tsx
+++ src/front-end/app/_layout.tsx
@@ -1,12 +1,17 @@
+import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import ThemeProvider from "@/context/ThemeContext";
 import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaView>
-      <Header />
-      <Slot />
+      <ThemeProvider>
+        <Header />
+        <Slot />
+        <Footer />
+      </ThemeProvider>
     </SafeAreaView>
   );
 }
--- src/front-end/components/shared/Navbar.tsx
+++ src/front-end/components/shared/Navbar.tsx
@@ -1,21 +1,27 @@
+import { useRouter } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Navbar() {
-    return <View style={styles.navbar}>
-        <Text style={styles.navbarTitle}>Steamgram</Text>
-        <Text>Login</Text>
+  const router = useRouter();
+
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.navbarTitle} onPress={() => router.navigate("/")}>
+        Steamgram
+      </Text>
     </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    navbar: {
-        marginHorizontal: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    navbarTitle: {
-        fontSize: 20,
-        fontWeight: "bold"
-    }
-})
+  navbar: {
+    marginHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  navbarTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
