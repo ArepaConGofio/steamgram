bd519f0ca5fe9eb0b856d0cbb773a9500d39df8d
strSalazar <alejandrosalazargonzalez2004@gmail.com>
2026-02-27 18:50:29 +0000

    refactor: services optimized using abstract classes

--- src/steamgram/src/main/java/com/arepacongofio/steamgram/service/LikeServiceImpl.java
+++ src/steamgram/src/main/java/com/arepacongofio/steamgram/service/LikeServiceImpl.java
@@ -1,7 +1,5 @@
 package com.arepacongofio.steamgram.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.arepacongofio.steamgram.models.Like;
--- src/steamgram/src/main/java/com/arepacongofio/steamgram/service/UserServiceImpl.java
+++ src/steamgram/src/main/java/com/arepacongofio/steamgram/service/UserServiceImpl.java
@@ -1,48 +1,15 @@
 package com.arepacongofio.steamgram.service;
 
-import java.util.List;
-
 import com.arepacongofio.steamgram.models.User;
 import com.arepacongofio.steamgram.repository.UserJpaRepository;
-import com.arepacongofio.steamgram.service.interfaces.IService;
+import com.arepacongofio.steamgram.service.abst.AbstractService;
 
-public class UserServiceImpl implements IService<User, Integer >{
+public class UserServiceImpl extends AbstractService<User, Integer >{
     
     UserJpaRepository repository;
 
     public UserServiceImpl(UserJpaRepository repository) {
-        this.repository = repository;
+        super(repository);
     }
-
-    @Override
-    public List<User> findAll() {
-        return repository.findAll();
-    }
-
-    @Override
-    public boolean existsById(Integer id) {
-        return repository.existsById(id);
-    }
-
-    @Override
-    public User findById(Integer id) {
-        return repository.findById(id).orElse(null);
-    }
-
-    @Override
-    public User save(User entity) {
-        return repository.save(entity);
-    }
-
-    @Override
-    public boolean deleteById(Integer id) {
-        if (!existsById(id)) {
-            return false;
-        }
-        User deleteUser =  findById(id);
-        repository.delete(deleteUser);
-        return true;
-    }
-
     
 }
