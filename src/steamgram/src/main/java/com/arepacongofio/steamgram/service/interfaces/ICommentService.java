503692c257795a2d716e3f5a9d506df64e429739
strSalazar <alejandrosalazargonzalez2004@gmail.com>
2026-03-12 15:46:08 +0000

    fix: name fixed

--- src/steamgram/src/main/java/com/arepacongofio/steamgram/service/interfaces/ICommentService.java
+++ src/steamgram/src/main/java/com/arepacongofio/steamgram/service/interfaces/ICommentService.java
@@ -3,6 +3,6 @@ package com.arepacongofio.steamgram.service.interfaces;
 import com.arepacongofio.steamgram.entities.Comment;
 import com.arepacongofio.steamgram.service.interfaces.generic.IGenericService;
 
-public interface ICommertService extends IGenericService<Comment, Integer>{
+public interface ICommentService extends IGenericService<Comment, Integer>{
     
 }
