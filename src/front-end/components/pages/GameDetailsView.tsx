9b8db99466827aee01ad9b897eca2fc46db78588
JesusLugo2002 <jesusdlugo02@gmail.com>
2026-04-15 22:04:11 +0100

    feat: add right header button generation into game details container

--- src/front-end/components/pages/GameDetailsView.tsx
+++ src/front-end/components/pages/GameDetailsView.tsx
@@ -1,10 +1,20 @@
 import { Game } from "@/models/Game";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 type Props = {
     game: Game;
 }
 
 export default function GameDetailsView({ game }: Props) {
-    return <Text>{game.name}</Text>
+    return (
+        <View>
+            
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
--- src/front-end/containers/GameDetailsContainer.tsx
+++ src/front-end/containers/GameDetailsContainer.tsx
@@ -1,5 +1,6 @@
 import GameDetailsView from "@/components/pages/GameDetailsView";
 import { Game } from "@/models/Game";
+import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "expo-router";
 import { useEffect } from "react";
 
@@ -10,8 +11,13 @@ type Props = {
 export default function GameDetailsContainer({ game }: Props) {
     const navigation = useNavigation();
 
+    const generateRightHeaderButton = () => {
+        // TODO: Implements logic of red heart when you liked it and be pressable to toggle like.
+        return <AntDesign name="heart" size={24}/>
+    }
+
     useEffect(() => {
-        navigation.setOptions({ title: game.name });
+        navigation.setOptions({ title: game.name, headerRight: generateRightHeaderButton });
     }, [navigation, game.name])
 
     return <GameDetailsView game={game}/>
