f6a6387d01fe4048d0f13b25d57264799f9fd4d0
strSalazar <alejandrosalazargonzalez2004@gmail.com>
2026-04-28 19:06:47 +0100

    fix: mapped added

--- src/steamgram/src/main/java/com/arepacongofio/steamgram/mappers/ReviewMapper.java
+++ src/steamgram/src/main/java/com/arepacongofio/steamgram/mappers/ReviewMapper.java
@@ -1,6 +1,7 @@
 package com.arepacongofio.steamgram.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.arepacongofio.steamgram.domain.responses.ReviewResponse;
 import com.arepacongofio.steamgram.domain.requests.ReviewCreateRequest;
@@ -9,6 +10,9 @@ import com.arepacongofio.steamgram.entities.Review;
 @Mapper
 public interface ReviewMapper {
     
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createDate", ignore = true)
+    @Mapping(target = "updateDate", ignore = true)
     Review toEntity(ReviewCreateRequest review);
 
     ReviewResponse toResponse(Review review);
